5bd02b9a386bfef7c003574e54926e764a15181d
Manideep <gujjulasaimanideepreddy@gmail.com>
2026-09-12 13:07:30 +0530

    Fix frontend API URLs for deployment

--- client/src/api/schemeApi.js
+++ client/src/api/schemeApi.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
-
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 export const matchSchemes = async (formData) => {
   const response = await axios.post(`${API_BASE_URL}/match`, formData);
   return response.data;
--- client/src/pages/SchemeDetail.jsx
+++ client/src/pages/SchemeDetail.jsx
@@ -9,8 +9,7 @@ import DocumentChecklist from '../components/DocumentChecklist';
 import SchemeImage from '../components/SchemeImage';
 import { localize, localizeBenefitType, localizeEligibility } from '../utils/localize';
 
-const API_BASE_URL = 'http://localhost:5000/api';
-
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 function SchemeDetail() {
   const { t, i18n } = useTranslation();
   const { id } = useParams();
